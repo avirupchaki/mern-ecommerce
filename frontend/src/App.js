88be421bebda8f26954c5204960bf83edb753247
Avirup Chaki <avirupchaki@Avirups-MacBook-Air.local>
2023-07-08 00:09:51 +0100

    First Commit

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">e-commerce</a>
+      </header>
+      <main>List products</main>
+    </div>
+  );
+}
+
+export default App;
